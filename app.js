23e73005a1c13b2e9e0b84eb7a7126c4f5ef9bcf
git <alencarjailson88@gmail.com>
2024-12-06 18:07:31 -0300

    Insert database and first tables

--- app.js
+++ app.js
@@ -13,6 +13,38 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = 3333;
 
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(flash());
+
+
+//create and/or conect database
+const db = new sqlite3.Database('lider_cestas.db', (err) => {
+    if (err) {
+        console.error(err.message);
+        throw err;
+    }
+    console.log('Sucess');
+});
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTERGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE,
+            function TEXT NOT NULL,
+            phone INTERGER NOT NULL
+        )
+    `)
+    db.run(`
+
+    `)
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +58,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
