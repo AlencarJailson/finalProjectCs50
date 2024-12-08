7a8a2f3dff35f2bb6724c774f294222fb62173b3
git <alencarjailson88@gmail.com>
2024-12-08 13:46:29 -0300

    Progress in building the database

--- db.js
+++ db.js
@@ -11,20 +11,11 @@ const db = new sqlite3.Database('lider_cestas.db', (err) => {
 
 const createTables= () => {
     db.run(`
-        CREATE TABLE IF NOT EXISTS permissions (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            permission TEXT NOT NULL
-        )
-    `);
-
-    db.run(`
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL,
-            permission_id INTEGER NOT NULL,
             phone TEXT NOT NULL,
-            FOREIGN KEY(permission_id) REFERENCES permissions(id)
         )
     `);
 
@@ -46,7 +37,10 @@ const createTables= () => {
             city_id INTEGER NOT NULL,
             cpf TEXT UNIQUE,
             observation TEXT,
-            FOREIGN KEY(city_id) REFERENCES cities(id)
+            seller_id INTEGER NOT NULL,
+            location TEXT,
+            FOREIGN KEY(city_id) REFERENCES cities(id),
+            FOREIGN KEY(seller_id) REFERENCES users(id)
         )
     `);
 
@@ -58,9 +52,26 @@ const createTables= () => {
     `);
 
     db.run(`
+        CREATE TABLE IF NOT EXISTS brands (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        )
+    `);
+
+    db.run(`
         CREATE TABLE IF NOT EXISTS products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             description TEXT NOT NULL
+        )
+    `);
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS bundle (
+            product_id INTEGER,
+            brand_id INTEGER,
+            bundle INTEGER
+            FOREIGN KEY(product_id) REFERENCES products(id)
+            FOREIGN KEY(brand_id) REFERENCES brands(id)
         )
     `);
 
@@ -82,11 +93,16 @@ const createTables= () => {
         CREATE TABLE IF NOT EXISTS shopping (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             product_id INTEGER NOT NULL,
-            brand TEXT NOT NULL,
+            brand_id INTERGER NOT NULL,
             amount INTEGER NOT NULL,
             date TEXT NOT NULL,
+            payment_id INTEGER,
+            reason_id INTEGER,
             supplier_id INTEGER NOT NULL,
             FOREIGN KEY(product_id) REFERENCES products(id),
+            FOREIGN KEY(brand_id) REFERENCES brands(id),
+            FOREIGN KEY(payment_id) REFERENCES payment_methods(id),
+            FOREIGN KEY(reason_id) REFERENCES reasons(id),
             FOREIGN KEY(supplier_id) REFERENCES suppliers(id)
         )
     `);
@@ -96,7 +112,7 @@ const createTables= () => {
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             customer_id INTEGER NOT NULL,
             product_id INTEGER NOT NULL,
-            brand TEXT NOT NULL,
+            brand_id TEXT NOT NULL,
             reason_id INTEGER NOT NULL,
             payment_id INTEGER NOT NULL,
             date TEXT DEFAULT CURRENT_DATE,
@@ -105,10 +121,12 @@ const createTables= () => {
             unit_value FLOAT NOT NULL,
             total_value FLOAT NOT NULL,
             seller_id INTEGER NOT NULL,
+            order TEXT NOT NULL,
             FOREIGN KEY(customer_id) REFERENCES customers(id),
+            FOREIGN KEY(brand_id) REFERENCES brands(id),
             FOREIGN KEY(product_id) REFERENCES products(id),
             FOREIGN KEY(reason_id) REFERENCES reasons(id),
-            FOREIGN KEY(payment_id) REFERENCES payment_methods(id),
+            FOREIGN KEY(payment_id) REFERENCES payment_methods(id)
             FOREIGN KEY(seller_id) REFERENCES users(id)
         )
     `);
@@ -137,17 +155,23 @@ const createTables= () => {
         CREATE TABLE IF NOT EXISTS config_basket (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             date TEXT DEFAULT CURRENT_TIMESTAMP,
-            product_id INTEGER NOT NULL,
+            basket_id INTEGER NOT NULL,
             amount INTEGER NOT NULL,
+            product_id INTERGER NOT NULL,
+            FOREIGN KEY(basket_id) REFERENCES baskets(id),
             FOREIGN KEY(product_id) REFERENCES products(id)
         )
     `);
 
     db.run(`
         CREATE TABLE IF NOT EXISTS receivables (
+            sale_id INTEGER NOT NULL,
             customer_id INTEGER NOT NULL UNIQUE,
-            to_receive FLOAT NOT NULL,
-            FOREIGN KEY(customer_id) REFERENCES customers(id)
+            total_purchase FLOAT NOT NULL,
+            levy_id INTEGER NOT NULL,
+            total_payments FLOAT NOT NULL,
+            FOREIGN KEY(customer_id) REFERENCES customers(id),
+            FOREIGN KEY(levy_id) REFERENCES levy(id)
         )
     `);
 
