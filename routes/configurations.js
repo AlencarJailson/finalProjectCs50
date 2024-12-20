a8e178c172084e085876d77bfe00c9bedc9cb98d
git <alencarjailson88@gmail.com>
2024-12-19 22:31:49 -0300

    edit Configurations route and form

--- routes/configurations.js
+++ routes/configurations.js
@@ -5,52 +5,32 @@ const db = new sqlite3.Database('lider_cestas.db');
 const flash = require('connect-flash');
 
 router.get('/', (req, res, next) => {
-    db.all('SELECT * FROM products', (err, products) => {
+    db.all('SELECT * FROM baskets JOIN products ON products.id = baskets.product_id', (err, baskets) => {
         if (err) {
             return next(err);
         }
-        db.all('SELECT * FROM baskets JOIN products ON products.id = baskets.product_id', (err, baskets) => {
+        db.all('SELECT * FROM products', (err, products) => {
             if (err) {
                 return next(err);
             }
-            db.all('SELECT * FROM config_basket JOIN baskets  ON config_basket.basket_id = baskets.id JOIN products ON products.id = baskets.product_id', (err, configurations) => {
-                if (err) {
-                    return next(err);
-                }
-                res.render('configurations', { title: 'Configurations', products: products, baskets: baskets, configurations: configurations });
-            });
+            res.render('configurations', { title: 'Configurations', baskets: baskets, products: products });
+            const { basket_id, date } = req.body;
+            if (basket_id){
+                console.log('logged')
+            }
+            if (basket_id && date) {
+                db.all('SELECT * FROM config_basket c1 JOIN baskets b1 ON c1.basket_id = b1.id JOIN products p1 ON b1.product_id = p1.id WHERE c1.basket_id = ? AND c1.date = ?',
+                    [basket_id, date], (err, configurations) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    res.render('configurations', { title: 'Configurations', configurations: configurations, selectedBasket: basket_id, selectedDate: date });
+                });
+            } else {
+                res.render('configurations', { title: 'Configurations', baskets: baskets, products: products, configurations: [], selectedBasket: null, selectedDate: null });
+            }
         });
     });
-});
-
-router.post('/', (req, res) => {
-    const { date, basket_id, amount, product_id } = req.body;
-    if (!date, !basket_id, !amount, !product_id) {
-        return res.status(403).redirect('/configurations');
-    }
-    db.run (`
-        INSERT INTO config_basket (date, basket_id, amount, product_id) VALUES (?, ?, ?, ?)`,
-        [date, basket_id, amount, product_id]
-    );
-    req.flash('sucess', 'Produto Inserido');
-    res.redirect('/configurations');
-});
-
-router.post('/delete', (req, res) => {
-    const { id } = req.body;
-    if (!id) {
-        return res.status(403).redirect('/configurations');
-    }
-    db.run(`
-        DELETE FROM config_basket WHERE id = ?`,
-            [id], function(err) {
-                if (err) {
-                    req.flash('errDelete', 'Erro na exclusão');
-                    return res.status(500).redirect('/configurations');
-                }
-            }
-    );
-    res.redirect('/configurations');
 });
 
 module.exports = router;
